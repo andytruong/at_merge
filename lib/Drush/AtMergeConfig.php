25d536dc420592ac3ea2ff2654f18c5cd186f141
Andy Truong <andy@toila.net>
2013-10-18 11:54:54 +0700

    Create AtMergeConfig.php

---
+++ lib/Drush/AtMergeConfig.php
@@ -0,0 +1,16 @@
+<?php
+namespace Drupal\at_merge\Drush;
+
+class AtMergeConfig {
+  /**
+   * Path to make file.
+   * @var string
+   */
+  public $make_file;
+
+  /**
+   * Name of new module
+   * @var string
+   */
+  public $new_module_name;
+}
